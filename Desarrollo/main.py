284586085518c2f78090d6d4cc49248fe48ca7a8
Pablo_Villalba <pabloandresvillalba97@gmail.com>
2026-05-23 18:41:48 -0300

    Pablo | Creando funcion para agregar pais

--- Desarrollo/main.py
+++ Desarrollo/main.py
@@ -1,1 +1,11 @@
+import csv
+#Funcion para agregar paises al .csv
+def agregar_pais():
+    #nuevo_pais=input("Ingrese el nombre del nuevo pais: ")
+    with open("Trabajo_Integrador_Programacion/desarrollo/paises.csv", "r", newline="", encoding="UTF-8") as paises:
+        lectura_diccionario=csv.DictReader(paises)
+        for linea in lectura_diccionario:
+            print(f"Pais: {linea['nombre']} Poblacion: {linea['poblacion']} Superficie: {linea['superficie']}km cuadrados Continente: {linea['continente']}")
 
+
+agregar_pais()
