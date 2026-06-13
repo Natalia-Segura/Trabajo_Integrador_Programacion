10b4f0ad42085c73113f40a4ea4739cde0258830
Pablo_Villalba <pabloandresvillalba97@gmail.com>
2026-06-12 22:12:35 -0300

    Agregamos lineas

--- validar.py
+++ validar.py
@@ -1,13 +1,28 @@
 def validar_entero(entero):
     try:
         entero = int(entero)
-        return True
+        if entero > 0:
+            return True
+        else:
+            print("El numerodebe ser mayor que 0")
     except ValueError:
         print("Ingrese un numero entero")
+    except Exception as error:
+        print("\tERROR. Se produjo un error: ", type(error).__name__)
     return False
 
-def validar_flotante():
-    pass
+def validar_flotante(flotante):
+    try:
+        flotante = float(flotante)
+        if flotante > 0:
+            return True
+        else:
+            print("Ingrese un numero mayor que 0")
+    except ValueError:
+        print("Ingrese un numero flotante")
+    except Exception as error:
+        print("\tERROR. Se produjo un error: ", type(error).__name__)
+    return False
 
 def validar_texto(palabra):
     separar=palabra.split()
